439d7329457e3f5b295756f4402e1073be52e181
lixiaolong <lxl@qq.com>
2017-05-22 00:47:50 +0800

    提交

--- NScript/NScript/nfile.h
+++ NScript/NScript/nfile.h
@@ -3,13 +3,28 @@
 
 #include <stdio.h>
 
+///文件预缓冲 用于缓冲读取文件字符
+#define FILEBUFFER 256
+
 typedef struct NFile
 {
 	FILE *m_fp;
+
+	char buffer[FILEBUFFER];
+	char *p;
+	int ncount;
 }NFile;
 
 extern void fileInitalize(NFile *nfile);
+///打开文件
+extern void fileOpen(NFile *nfile, char *fileName);
+///读取一个字符
 extern char fileGetChar(NFile *nfile);
 extern void fileDestory(NFile *nfile);
+/*
+ * 从文件中预先读取字符
+ * nCount - 表示预先读取的字符个数
+ */
+extern char *filePreRead(NFile *nfile, int nCount);
 
 #endif
--- NScript/NScript/nobject.c
+++ NScript/NScript/nobject.c
@@ -1,2 +1,42 @@
 #include "nobject.h"
 
+void initStack(NStack *nstack)
+{
+	nstack->currIndex = nstack->stack;
+}
+
+void pushStack(NStack *nstack, NObject *obj)
+{
+	*(nstack->currIndex) = obj;
+	nstack->currIndex++;
+}
+
+NObject *popStack(NStack *nstack)
+{
+	NObject *r = *nstack->currIndex;
+	nstack->currIndex--;
+
+	return r;
+}
+
+void setObjectType(NObject obj, int type)
+{
+	obj.ttype = type;
+}
+
+int isObjectType(NObject obj, int type)
+{
+	if (obj.ttype == type)
+		return 1;
+
+	return 0;
+}
+
+void setObjTo2(NObject obj, NObject other)
+{
+	if (&obj == &other)
+		return ;
+
+	obj.Value = other.Value;
+	obj.ttype = other.ttype;
+}
